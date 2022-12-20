cf166bf39f9501ea82cc8877ede2c2bb7f71339a
PhiVerbiest <220502@student.glu.nl>
2022-12-20 11:03:36 +0100

    klaar

---
+++ Workshop_1/assets/javasccript/script.js
@@ -0,0 +1,61 @@
+
+console.log(eersteVariabele);
+
+// plak hier de 3 bovenstaande variabelen aan elkaar, met spaties
+var voornaam = "Phi "; // wijzig dit in jouw eigen voornaam
+var tussenvoegsel = " "; // wijzig dit in jouw eigen eventuele tussenvoegsel
+var achternaam = "Verbiest"; // wijzig dit in jouw eigen achternaam
+var volledigeNaam = voornaam + tussenvoegsel + achternaam;
+
+document.write("Mijn naam is: ", volledigeNaam);
+
+// bereken hier jouw leeftijd op basis van de eerder ingevulde geboortegegevens.
+var geboortedag = "3"; // je geboortedag
+var geboortemaand = "Februari"; // je geboortemaand
+var geboortejaar = "2005"; // je geboortejaar
+var huidig_jaartal = "2022"// het huidige jaar;
+var leeftijd = huidig_jaartal -= geboortejaar;
+
+document.write("<br>")
+document.write("Mijn leeftijd is: ", leeftijd);
+
+// bereken hier hoeveelste dag van dit kalenderjaar het is
+var dag_van_het_jaar = 0;
+
+function myFunction(){
+        // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
+    
+        var straat = "Hellemanstraat"; // je straat;
+        var huisnummer = "76"; // je huisnummer;
+        var woonplaats = "Deurne"; // je woonplaats;
+        var woonachtig_in_utrecht = "Utrecht";
+    
+        if (woonplaats === woonachtig_in_utrecht) {
+            document.write("<br>");
+            document.write("Woont redelijk in de buurt ");
+        } else {
+            document.write("<br>");
+            document.write("Woont niet in de buurt van Utrecht");
+        }
+}
+
+
+// bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+var aantal_studenten_in_deze_klas = 23;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = aantal_studenten_in_deze_klas *= kosten_van_frikandelbroodje *= 2;
+
+document.write("<br>")
+document.write("Totale lunch kosten: ", totale_lunchkosten);
+
+var eersteVariabele = "test";
+
+/* toon de waardes hieronder op het scherm gescheiden door het html: '<br>' element voor een nieuwe-regel
+  bijvoorbeeld:
+  document.write("Voornaam: " + voornaam);
+  document.write("<br>");
+  document.write("Achternaam: " + achternaam);
+  enzovoorts, zorg dat alle bovenstaande variabelen op het scherm komen
+*/
+
+myFunction()
---
+++ Workshop_2/assets/js/script.js
@@ -0,0 +1,44 @@
+document.getElementById("c").onclick = function(){ // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+
+document.getElementById("b").onclick = function(){
+	var b_toets = document.getElementById("audio_b");
+	b_toets.play();
+}
+
+document.getElementById("Bb").onclick = function(){
+	var Bb_toets = document.getElementById("audio_Bb");
+	Bb_toets.play();
+}
+
+document.getElementById("a").onclick = function(){
+	var a_toets = document.getElementById("audio_a");
+	a_toets.play();
+}
+
+document.getElementById("Ab").onclick = function(){
+	var Ab_toets = document.getElementById("audio_Ab");
+	Ab_toets.play();
+}
+document.getElementById("g").onclick = function(){
+	var g_toets = document.getElementById("audio_g");
+	g_toets.play();
+}
+document.getElementById("Gb").onclick = function(){
+	var Gb_toets = document.getElementById("audio_Gb");
+	Gb_toets.play();
+}
+document.getElementById("f").onclick = function(){
+	var f_toets = document.getElementById("audio_f");
+	f_toets.play();
+}
